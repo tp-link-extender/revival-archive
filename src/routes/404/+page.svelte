10c6f8cf8d739e26aec8328c91d0246c6a647029
Lewin Kelly <heli@odex.cf>
2023-07-19 17:34:23 +0100

    Add 404 page for static site in Netlify

---
+++ src/routes/404/+page.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+</script>
+
+<svelte:head>
+	<meta name="description" content="Page not found" />
+	<title>Not found • Revival Archive</title>
+</svelte:head>
+
+<div class="flex flex-col items-center justify-center">
+	<div class="bg-a px-22 rounded-4 mt-30vh p-8 text-center">
+		<h1 class="text-2.2rem m-0 mb-5">Error 404</h1>
+		<p class="mb-1">
+			<strong>Page not found</strong>
+		</p>
+		<a href="/">Head home?</a>
+	</div>
+</div>
