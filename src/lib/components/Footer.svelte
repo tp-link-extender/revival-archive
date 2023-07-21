5725fb06b4d71756c1347f3cbe346181e0bacb38
Lewin Kelly <heli@odex.cf>
2023-07-21 23:50:36 +0100

    Update footer and fix links

--- src/lib/components/Footer.svelte
+++ src/lib/components/Footer.svelte
@@ -1,10 +1,12 @@
 <footer class="mt-a text-#8c8b8a @light:text-#4c4b4a p-5 pt-12 text-center">
 	<a href="/" class="pe-1.5">Revival Archive</a>
-	2023-2023
+	2023
 	<nav class="mb-4 mt-5 text-white">
 		<a href="/about">About</a>
 		|
-		<a href="https://discord.gg/zfC3VH2WBa">Discord</a>
+		<a href="https://discord.gg/zfC3VH2WBa" target="_blank" >Discord</a>
+		|
+		<a href="https://github.com/tp-link-extender/revival-archive" target="_blank" >Github</a>
 	</nav>
 </footer>
 
