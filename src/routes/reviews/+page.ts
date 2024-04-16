4ceadaffd17dd3d3bcfb02433c3d00d5f5233db3
Lewin Kelly <heli@odex.cf>
2024-04-16 14:07:52 +0100

    Final build fixes

--- src/routes/reviews/+page.ts
+++ src/routes/reviews/+page.ts
@@ -1,20 +1,18 @@
 import type { RevivalMetadata } from "$lib/types"
 
-export async function load() {
-	const allPostFiles = import.meta.glob("../../../pages/revivals/*.md")
+const allPostFiles = import.meta.glob("../../../pages/revivals/*.md")
 
-	return {
-		reviews: Promise.all(
-			Object.keys(allPostFiles).map(async path => {
-				const { metadata } = (await allPostFiles[path]()) as {
-					metadata: RevivalMetadata
-				}
+export const load = async () => ({
+	reviews: await Promise.all(
+		Object.keys(allPostFiles).map(async path => {
+			const { metadata } = (await allPostFiles[path]()) as {
+				metadata: RevivalMetadata
+			}
 
-				return {
-					...metadata,
-					path: path.match(/(\w+)\.md/)?.[1],
-				}
-			})
-		),
-	}
-}
+			return {
+				...metadata,
+				path: path.match(/(\w+)\.md/)?.[1],
+			}
+		})
+	),
+})
